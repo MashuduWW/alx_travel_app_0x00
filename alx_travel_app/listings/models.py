d4b12e65e6716bf6de20d47b4058c9c4d1e87cd8
Mashudu <62095778+MashuduWW@users.noreply.github.com>
2025-07-20 22:03:25 +0200

    Add files via upload

---
+++ alx_travel_app/listings/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Listing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    price_per_night = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+    listing = models.ForeignKey(Listing, related_name='bookings', on_delete=models.CASCADE)
+    guest_name = models.CharField(max_length=100)
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    def __str__(self):
+        return f"{self.guest_name} - {self.listing.title}"
+
+
+class Review(models.Model):
+    listing = models.ForeignKey(Listing, related_name='reviews', on_delete=models.CASCADE)
+    reviewer_name = models.CharField(max_length=100)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.reviewer_name} - {self.rating}/5"
